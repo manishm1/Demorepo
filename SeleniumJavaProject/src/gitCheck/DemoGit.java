8abe376f1596581820b4800730fafe705b5a8e11
manishm1 <immanishit@gmail.com>
2023-05-02 21:28:08 -0400

    3rd line added in class demogit

--- SeleniumJavaProject/src/gitCheck/DemoGit.java
+++ SeleniumJavaProject/src/gitCheck/DemoGit.java
@@ -5,7 +5,8 @@ public class DemoGit {
 	public static void main(String[] args) 
 	{
 	System.out.println("Git Sample demo ");
-	System.out.println("Git Sample demo2 ");
+	System.out.println("Git Sample demo2nd line ");
+	System.out.println("Git Sample demo3rd line ");
 	
 	}
 
